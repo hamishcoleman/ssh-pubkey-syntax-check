f18f831158799c834c6d50542623652d9fc6d98e
Hamish Coleman <hamish@zot.org>
2018-02-20 22:40:25 +0800

    Move the core of the binary checker into a library and add test coverage

--- lib/SSH/Key.pm
+++ lib/SSH/Key.pm
@@ -5,6 +5,96 @@ use strict;
 #
 #
 
+use MIME::Base64;
+
+sub new {
+    my $class = shift;
+    my $self = {};
+    bless $self, $class;
+    return $self;
+}
+
+sub parse {
+    my $self = shift;
+    my $encoded = shift;
+
+    # we are passed the base64 encoded text of the key.
+    # This is an array of len,val pairs (len is u32 value)
+
+    # TODO
+    # - use a better base64 function that actually reports errors
+    # - if the len pointer is ever beyond the remaining length then we fail
+    # - pair 1 looks like the same as the string type
+
+    # for ecdsa-sha2-nistp256
+    # - pair 2 is a second string value
+    # - pair 3 is binary
+
+    # for ssh-rsa
+    # - pair 2 is "01 00 01"
+    # - pair 3 is binary of length 0101
+    # - pair 4 is ?
+    # - pair 5 is ?
+    # - pair 6 is ?
+
+    if (!defined($encoded)) {
+        $self->error("Base64 key missing");
+        return undef;
+    }
+
+    my $binary = decode_base64($encoded);
+    if (!defined($binary)) {
+        # never happens with this decode library
+        $self->error("Base64 key decode error");
+        return undef;
+    }
+
+    if (!length($binary)) {
+        $self->error("key length error");
+        return undef;
+    }
+
+    my $val;
+    my @fields;
+    while (length($binary)) {
+        ($val,$binary) = _extract_next_keypart($binary);
+        if (!defined($val)) {
+            $self->error("key structure length/val error");
+            return undef;
+        }
+        push @fields,$val;
+    }
+
+    # TODO - check the none name fields against correct values?
+
+    # TODO - check number of fields found
+    # - escda == 3?
+    # - ssh-rsa == 6?
+
+    $self->{encoded} = $encoded;
+    $self->{fields} = \@fields;
+    $self->{type} = $fields[0];
+
+    delete $self->{error};
+    return $self;
+}
+
+# get the type of the key
+sub type {
+    my $self = shift;
+    return $self->{type};
+}
+
+# get/set error message
+sub error {
+    my $self = shift;
+    my $message = shift;
+    if (defined($message)) {
+        $self->{error} = $message;
+    }
+    return $self->{error};
+}
+
 # Return a list of valid ssh key types
 sub _keytypes {
     my @list = `ssh -Q key 2>/dev/null`;
--- lib/SSH/Key.t
+++ lib/SSH/Key.t
@@ -6,8 +6,10 @@ use strict;
 my $class = "SSH::Key";
 use_ok($class);
 
+# not really a test of the correct output...
 ok(scalar(SSH::Key::_keytypes())>0);
 
+# Test the internal key part extractor
 is(SSH::Key::_extract_next_keypart('123'), undef, 'Runt key');
 is(SSH::Key::_extract_next_keypart("\x00\x00\x00\x0bbad"), undef, 'Runt val');
 
@@ -15,3 +17,19 @@ my $binary = "\x00\x00\x00\x0bssh-ed25519abab";
 my ($val, $remain) = SSH::Key::_extract_next_keypart($binary);
 is($val, 'ssh-ed25519');
 is($remain, 'abab');
+
+# Test the object interface
+my $obj = new_ok($class);
+
+is($obj->parse(), undef, "missing an encoded value is an error");
+is($obj->error(), "Base64 key missing");
+
+is($obj->parse('<>'), undef, "invalid base64 is an error");
+is($obj->error(), "key length error");
+
+is($obj->parse('AAAAC3NzaC1lZD'), undef, "truncated structure is an error");
+is($obj->error(), "key structure length/val error");
+
+is($obj->parse('AAAAC3NzaC1lZDI1NTE5AAAAIOqTtpQkv6bgHxWhr4QGf7WOY1760ArL/asGL3Lxqhlr'), $obj);
+
+is($obj->type(), 'ssh-ed25519');
